baa86e219fae151a541f36864bbcd86871c43223
akash wakade <mr.akashwakade@gmail.com>
2023-08-10 18:02:46 +0530

    added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT || 8000
-
+const cors=require("cors")
+app.use(cors())
 server.use(middlewares)
 server.use(router)
 server.listen(PORT, () => {
